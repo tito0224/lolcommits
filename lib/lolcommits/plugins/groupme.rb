969366014c50a6b9a31c234b53455d030bb80ad9
John Lotito <johnlotito@gmail.com>
2014-01-28 22:49:01 -0500

    WORKSgit statusgit statusgit status

--- lib/lolcommits/plugins/groupme.rb
+++ lib/lolcommits/plugins/groupme.rb
@@ -1,47 +1,61 @@
 require 'rest_client'
+require 'json'
 
 module Lolcommits
   class Groupme < Plugin
-    attr_accessor :groupme_bot_token 
-    attr_accessor :groupme_access_token
+    attr_accessor :bot_token 
+    attr_accessor :access_token
 
     IMAGE_SERVICE_URL = "https://image.groupme.com/images"
     BOT_URL = "https://api.groupme.com/v3/bots/post"
 
     def initialize(runner)
       super
-      self.options << 'groupme_bot_token'
-      self.options << 'groupme_access_token'
+      self.options << 'bot_token'
+      self.options << 'access_token'
     end
 
     def run
+      debug "in run"
       return unless valid_configuration?
 
       repo = self.runner.repo.to_s
+      debug "repo: #{repo}"
+
+      debug "config: bot_token=" + configuration["bot_token"]
+      debug "config: access_token=" + configuration["access_token"]
+
       if not repo.empty?
         # upload image to image service
         response = RestClient.post(
           IMAGE_SERVICE_URL, 
           { :file => File.new(self.runner.main_image) },
-          { "X-Access-Toke" => configuration["groupme_access_token"] }
+          { "X-Access-Token" => configuration["access_token"] }
         )
+        debug "#{response}"
+        
+        response = JSON.parse(response)
+        image_url = response["payload"]["url"]
+        debug "#{image_url}"
 
-        image_url = response.payload.url
-
-        RestClient.post(
-          BOT_URL, 
-          {
-            :bot_id => configuration["groupme_bot_token"],
+        body = {
+            :bot_id => configuration["bot_token"],
             :text => self.runner.message,
-            :attachments => { :type => "image", :url => image_url }
-          }
+            :attachments => [{ :type => "image", :url => image_url }]
+          }.to_json
+        debug body
+
+        message_response = RestClient.post(
+          BOT_URL, 
+          body,
+          {:content_type => :json, :accept => :json}
         )
+        debug message_response
       end
     end
 
     def is_configured?
-      !configuration["enabled"].nil? && configuration["groupme_bot_token"] 
-        && configuration["groupme_access_token"]
+      configuration["bot_token"] && configuration["access_token"]
     end
 
     def self.name
